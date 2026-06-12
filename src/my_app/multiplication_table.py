02f574b131e667234606dd75957247ab9567729b
Lena S <lena.i.sandberg@gmail.com>
2026-06-12 14:49:50 +0200

    Multiplikationstabellen

---
+++ src/my_app/multiplication_table.py
@@ -0,0 +1,28 @@
+# Multiplikationstabell
+# Funktion ska ge oss multiplikationstabellen.
+# Parametern "n" talar om vilket tals tabell vi ska skapa.
+# Parametern "limit" talar om var vi ska sluta multiplicera.
+# Exempel 3:ans tabell, med limit==2, ska programmet räkna ut:3*1 = 3, 3*2 = 6
+# multiplication_table(3, 2) → [3, 6]
+
+# Formulera krav
+# som användare
+# vill jag kunna ange vilket tal och antal multiplikationer (med start från 1)
+# så att jag får en lista med talets uträknade mulitplikationstabell
+
+def multiplication_table(n, limit):
+    # provar att sätta båda valideringarna för int i samma if-sats
+    # då blir inte exakt position utpekad utan ges generellt meddelande om int.
+    if not isinstance(n, int) or not isinstance(limit, int):
+        raise TypeError("Parametrar måste vara av typen int.")
+
+    multiplication_list = []
+
+    for i in range(1, limit+1):
+        value = n*i
+        multiplication_list.append(value)
+        i += 1
+    return multiplication_list
+
+# TODO hur hantera om parmetrar saknas?
+print(multiplication_table(2, 3))
---
+++ tests/test_multiplication_table.py
@@ -0,0 +1,37 @@
+# ACCEPTANSKRITERIER
+# AK1: om tal n eller limit rader inte är en int ska funktionen returnera TypeError
+# AK2: om n och limit är int ska funktionen returnera en utskrift av multiplikationstabellen enligt limit
+# AK3: om limit > 100 så ska funktionen returnera False
+
+
+from my_app.multiplication_table import *
+import pytest
+
+def test_multiplication_table__n_is_not_int():
+    # Arrange
+    n_not_int = "12a"
+    limit_is_int = 3
+    # act & assert
+    with pytest.raises(TypeError):
+        multiplication_table(n_not_int, limit_is_int)
+
+def test_multiplication_table__limit_is_not_int():
+    # Arrange
+    limit_not_int = "12b"
+    n_is_int = 5
+    # act & assert
+    with pytest.raises(TypeError):
+        multiplication_table(n_is_int, limit_not_int)
+
+def test_multiplication_table__multiply():
+    # arrange
+    n = 4
+    limit = 2
+
+    # act
+    expected = [4, 8]
+    actual = multiplication_table(n, limit)
+
+    # assert
+    assert actual == expected
+
